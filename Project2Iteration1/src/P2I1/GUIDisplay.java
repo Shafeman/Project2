69fe0e0eb2e448d977526bd8a4b62c561d2b05ae
Shafeman <amazingshafeman@yahoo.com>
2015-07-11 16:28:41 -0500

    Initial Commit with RefridgeratorDisplay interface and barebones (no code) GUI display.

---
+++ Project2Iteration1/src/P2I1/GUIDisplay.java
@@ -0,0 +1,75 @@
+package P2I1;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class GUIDisplay extends JFrame implements ActionListener, RefridgeratorDisplay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setRefridgerator() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnFridgeLightOn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnFridgeLightOff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnFreezerLightOn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnFreezerLightOff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void freezerDoorClosed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void freezerDoorOpen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fridgeDoorClosed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fridgeDoorOpen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Project2Iteration1/src/P2I1/RefridgeratorDisplay.java
@@ -0,0 +1,15 @@
+package P2I1;
+
+public interface RefridgeratorDisplay {
+	
+	public void setRefridgerator(/*refridge class*/);
+	public void turnFridgeLightOn();
+	public void turnFridgeLightOff();
+	public void turnFreezerLightOn();
+	public void turnFreezerLightOff();
+	public void freezerDoorClosed();
+	public void freezerDoorOpen();
+	public void fridgeDoorClosed();
+	public void fridgeDoorOpen();
+
+}
