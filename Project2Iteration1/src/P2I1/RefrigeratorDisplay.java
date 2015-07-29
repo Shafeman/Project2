b3dfbe31b4d92164a0d0af1f5f87bdcb57961711
Jonathan Franco <endallbeall@live.com>
2015-07-28 23:33:50 -0500

    Converted the RefrigeratorDisplay.java from an interface to an abstract class that extends Observable

--- Project2Iteration1/src/P2I1/RefrigeratorDisplay.java
+++ Project2Iteration1/src/P2I1/RefrigeratorDisplay.java
@@ -1,24 +1,99 @@
-/**
- * Barbarians: Douglas Brian Shaffer, Jonathan Franco
- * The RefrigeratorDisplay is an interface for the GUIDisplay to implement.
- * It defines methods for setting the Refrigerator, controlling the lights
- * and notifying the GUI of the state and temperatures.
- */
+
 package P2I1;
 
-public interface RefrigeratorDisplay {
+import java.util.Observable;
+
+public abstract class RefrigeratorDisplay extends Observable{
 	
-	public void setRefrigerator(Refrigerator refrigerator);
-	public void turnFridgeLightOn();
-	public void turnFridgeLightOff();
-	public void turnFreezerLightOn();
-	public void turnFreezerLightOff();
-	public void freezerCooling();
-	public void freezerIdle();
-	public void fridgeCooling();
-	public void fridgeIdle();
-	public void updateFridgeTemp(int temp);
-	public void updateFreezerTemp(int temp);
-	public void setStartTemps(int room, int fridge, int freezer);
+	protected static RefrigeratorContext context;
+	protected static RefrigeratorDisplay instance;
+	
+	
+	/**
+	 * Initialized the context and instance
+	 */
+	protected RefrigeratorDisplay() {
+		instance = this;
+		context = RefrigeratorContext.instance();
+	}
+	
+	/**
+	 * Singleton
+	 * 
+	 * @return the instance
+	 */
+	public static RefrigeratorDisplay instance() {
+		return instance;
+	}
+	
+	/**
+	 * Do the initialization to make the context an observer
+	 */
+	public void initialized() {
+		context.initialize();
+	}
+	
+	public abstract void setRefrigerator(Refrigerator refrigerator);
+	
+	
+	/**
+	 * Notify that the fridge light is on
+	 */
+	public abstract void turnFridgeLightOn();
+	
+	/**
+	 * Notify that the fridge light is off
+	 */
+	public abstract void turnFridgeLightOff();
+	
+	/**
+	 * Notify that the freezer light is on
+	 */
+	public abstract void turnFreezerLightOn();
+	
+	/**
+	 * Notify that the freezer light is off
+	 */
+	public abstract void turnFreezerLightOff();
+	
+	/**
+	 * Notify that the freezer is cooling
+	 */
+	public abstract void freezerCooling();
+	
+	/**
+	 * Notify that the freezer is NOT cooling
+	 */
+	public abstract void freezerIdle();
+	
+	/**
+	 * Notify that the fridge is cooling
+	 */
+	public abstract void fridgeCooling();
+	
+	/**
+	 * Notify that the fridge is NOT cooling
+	 */
+	public abstract void fridgeIdle();
+	
+	/**
+	 * Updates the Fridge temperature
+	 * @param temp 
+	 */
+	public abstract void updateFridgeTemp(int temp);
+	
+	/**
+	 * Updates the Freezer temperature
+	 * @param temp
+	 */
+	public abstract void updateFreezerTemp(int temp);
+	
+	/**
+	 * Sets the temperatures at the start of the program
+	 * @param room
+	 * @param fridge
+	 * @param freezer
+	 */
+	public abstract void setStartTemps(int room, int fridge, int freezer);
 
 }
