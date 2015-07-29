5cbf4cc5d62f17cdbc6582defceb7d2c69306c70
Jonathan Franco <endallbeall@live.com>
2015-07-28 23:12:54 -0500

    Abstract class to help get rid of conditionals.

---
+++ Project2Iteration1/src/P2I1/GUIButton.java
@@ -0,0 +1,27 @@
+package P2I1;
+
+import javax.swing.JButton;
+
+/**
+ * Code from the book, This abstract class 
+ * helps get rid of conditionals 
+ */
+public abstract class GUIButton extends JButton{
+
+	
+	/**
+	 * Create button with the proper text
+	 * @param string : button text
+	 */
+	public GUIButton(String string) {
+		super(string);
+	}
+	
+	/**
+	 * To inform the listener that the button has been clicked
+	 * @param display : the GUI
+	 */
+	public abstract void inform(RefrigeratorDisplay display);
+		
+	
+}
