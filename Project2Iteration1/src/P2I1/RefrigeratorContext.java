4d286f169537cbb34174acc198ada20c944ed232
Jonathan Franco <endallbeall@live.com>
2015-07-29 00:08:24 -0500

    Set up the RefrigeratorContext

---
+++ Project2Iteration1/src/P2I1/RefrigeratorContext.java
@@ -0,0 +1,43 @@
+package P2I1;
+
+public class RefrigeratorContext {
+	private static RefrigeratorDisplay refrigeratorDisplay;
+	private RefrigeratorState currentState;
+	private static RefrigeratorContext instance;
+	
+	/**
+	 * Singleton
+	 */
+	private RefrigeratorContext() {
+		instance = this;
+		refrigeratorDisplay = RefrigeratorDisplay.instance();
+		currentState = DoorClosedCoolingState.instance();
+	}
+	
+	/**
+	 * Return the instance
+	 * 
+	 * @return RefrigeratorContext()
+	 */
+	public static RefrigeratorContext instance() {
+		if (instance == null) {
+			instance = new RefrigeratorContext();
+		}
+		return instance;
+	}
+	
+	public void initialize() {
+		instance.changeCurrentState(DoorClosedCoolingState.instance());
+	}
+	
+	public void changeCurrentState(RefrigeratorState nextState) {
+		currentState.leave();
+		currentState = nextState;
+		nextState.run();
+	}
+	
+	public RefrigeratorDisplay getDisplay() {
+		return refrigeratorDisplay;
+	}
+	
+}
